2a490283fa54d0dcf54c125742ca2dd1d5d8f087
Andrey-6oyko <andreyboyko200032@gmail.com>
2024-05-28 08:17:22 +0300

    First

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let n = document.getElementById("a1")
+let n_1 = document.getElementById("a2")
+let n_2 = document.getElementById("a3")
+let n_3 = document.getElementById("a4")
+let n_4 = document.getElementById("a5")
+
+n.addEventListener("click", (event) => {
+  alert("Кожному мила своя сторона. ")
+});
+
+n_1.addEventListener("click", (event) => {
+  alert("Усвідомлена любов до свого народу не поєднується з ненавистю до інших. ")
+});
+
+n_2.addEventListener("click", (event) => {
+  alert("Ті, що виїздять за море, міняють небо, а не душу. ")
+});
+
+n_3.addEventListener("click", (event) => {
+  alert("Для нас дорогі батьки, дорогі діти, близькі, родичі; але всі уявлення про любов до чого-небудь поєднані в одномуслові «вітчизна». ")
+});
+
+n_4.addEventListener("click", (event) => {
+  alert("Мова — це глибина тисячоліть.")
+});
